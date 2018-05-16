c09cc453e96bf493b7ab6cbc9f024200fd4166de
Qristin <Qristin@aua.am>
2018-05-17 01:55:31 +0500

    Ininitial commit

---
+++ Med.main.py
@@ -0,0 +1,28 @@
+import json
+
+#function: that open your json file, hospital_data
+
+#function:
+#print list of diseases
+#take user inputted disease (this should be done in main function)
+#print symptoms of the disease and ask user if they have them
+#if user says yes,
+#check each hospital and see which hospitals can treat that disease
+#next, check the hospitals that can treat the disease, and check if they have
+#the diagnosis tools needed for the disease
+#if the hospital has both the disease treatment and the diagnosis tools, print
+#the hospital information
+
+def main():
+	with open("Med_Care.json") as "data _ file":
+	compare data
+ return
+
+
+
+def main():
+    #run the function that opens hospital_data
+    #get disease from user (make sure to print list of diseases for them first)
+    #run function that takes user input and outputs diagnosis and hospital information
+
+main()
